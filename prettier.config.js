eeb18126a452c0d96db889c0747055a55db52d20
vandercbl <62650015+vandercbl@users.noreply.github.com>
2020-07-09 22:21:34 -0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  allowParens: 'avoid',
+  semi: false, // retira os ; do final das linhas
+  printWidth: 100,
+  useTabs: true,
+  tabWidth: 2,
+  endOfLine: 'lf'
+}
